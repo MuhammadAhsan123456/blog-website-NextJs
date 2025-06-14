28feb20f109ae7b945ece61024b0be28cbe3933d
MuhammadAhsan123456 <ahsan663399@gmail.com>
2025-06-15 02:40:25 +0500

    my first blog website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-6xl font-bold text-center">Tailwind Css</h1>
+      <h1 className="text-6xl font-bold text-center">Tailwind Css</h1>
     </>
   );
 }
